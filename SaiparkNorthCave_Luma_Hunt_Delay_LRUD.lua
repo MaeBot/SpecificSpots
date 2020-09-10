1d628ae9bed76c2be04b549969f5d393d4a795b0
MaeBot <69312348+MaeBot@users.noreply.github.com>
2020-09-10 21:18:33 +0200

    Update SaiparkNorthCave_Luma_Hunt_Delay_LRUD.lua

--- SaiparkNorthCave_Luma_Hunt_Delay_LRUD.lua
+++ SaiparkNorthCave_Luma_Hunt_Delay_LRUD.lua
@@ -51,13 +51,12 @@ if tblua:IsInWorld() == true then
      elseif tblua:GetPixelColor(1150, 100) == "0x002245" then
      local securitytopr = math.random(621, 945)
      local securityfefeee = math.random(242, 354)
-     tblua:KeyDown(0x26)
+     tblua:KeyDown(0x28)
      tblua:Sleep(securityfefeee)
+     tblua:KeyUp(0x26)
      tblua:KeyDown(0x27)
      tblua:Sleep(securitytopr)
      tblua:KeyUp(0x27)
-     tblua:Sleep(securityfefeee)
-     tblua:KeyUp(0x26)
      elseif tblua:GetPixelColor(1210, 65) == "0x002245" then
      local securitydowne = math.random(242, 354)
      local securityleft = math.random(647, 981)
@@ -69,14 +68,15 @@ if tblua:IsInWorld() == true then
      tblua:Sleep(securityleft)
      tblua:KeyUp(0x25)
      elseif tblua:GetPixelColor(1175, 150) == "0x002245" then
-     local securitytop = math.random(621, 945)
-     local securityfefee = math.random(242, 354)
-     tblua:KeyDown(0x28)
+     local securitytop = math.random(1021, 1545)
+     local securityfefee = math.random(242, 504)
+     tblua:KeyDown(0x26)
+     tblua:Sleep(securityfefee)
+     tblua:KeyDown(0x25)
+     tblua:Sleep(securitytop)
+     tblua:KeyUp(0x25)
      tblua:Sleep(securityfefee)
      tblua:KeyUp(0x26)
-     tblua:KeyDown(0x27)
-     tblua:Sleep(securitytop)
-     tblua:KeyUp(0x27)
      elseif MovementSwitch == 1 then
        local lol = math.random(5)
        if c > lol then
