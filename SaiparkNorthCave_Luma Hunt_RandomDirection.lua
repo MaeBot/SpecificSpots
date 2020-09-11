76e406b639d684505059d556c768c351c50feb78
MaeBot <69312348+MaeBot@users.noreply.github.com>
2020-09-11 23:04:36 +0200

    Update SaiparkNorthCave_Luma Hunt_RandomDirection.lua

--- SaiparkNorthCave_Luma Hunt_RandomDirection.lua
+++ SaiparkNorthCave_Luma Hunt_RandomDirection.lua
@@ -15,8 +15,6 @@ tblua:GetAreaColor()
 tblua:Sleep(1000)
 
 MovementSwitch = 1
-c = 0
-s = 0
 t = 0
 
 if tblua:IsInWorld() == true then
@@ -122,41 +120,89 @@ if tblua:IsInWorld() == true then
        tblua:KeyDown(0x25)
        tblua:Sleep(Mov1)
        tblua:KeyUp(0x25)
-       c = c + 1
-       s = s + 1
        t = t + 1
-       local MovS1 = math.random(8)
-       MovementSwitch = MovS1
+       local MovS1 = math.random(7)
+       if MovS1 == 1 then
+       MovementSwitch = 2
+       elseif MovS1 == 2 then
+       MovementSwitch = 3
+       elseif MovS1 == 3 then
+       MovementSwitch = 4
+       elseif MovS1 == 4 then
+       MovementSwitch = 5
+       elseif MovS1 == 5 then
+       MovementSwitch = 6
+       elseif MovS1 == 6 then
+       MovementSwitch = 7
+       elseif MovS1 == 7 then
+       MovementSwitch = 8
+       end
      elseif MovementSwitch == 2 then
        local Mov2 = math.random(123, 653)
        tblua:KeyDown(0x26)
        tblua:Sleep(Mov2)
        tblua:KeyUp(0x26)
-       c = c + 1
-       s = s + 1
        t = t + 1
-       local MovS2 = math.random(8)
-       MovementSwitch = MovS2
+       local MovS2 = math.random(7)
+       if MovS2 == 1 then
+       MovementSwitch = 1
+       elseif MovS2 == 2 then
+       MovementSwitch = 3
+       elseif MovS2 == 3 then
+       MovementSwitch = 4
+       elseif MovS2 == 4 then
+       MovementSwitch = 5
+       elseif MovS2 == 5 then
+       MovementSwitch = 6
+       elseif MovS2 == 6 then
+       MovementSwitch = 7
+       elseif MovS2 == 7 then
+       MovementSwitch = 8
+       end
      elseif MovementSwitch == 3 then
        local Mov3 = math.random(114, 641)
        tblua:KeyDown(0x27)
        tblua:Sleep(Mov3)
        tblua:KeyUp(0x27)
-       c = c + 1
-       s = s + 1
        t = t + 1
-       local MovS3 = math.random(8)
-       MovementSwitch = MovS3
+       local MovS3 = math.random(7)
+       if MovS3 == 1 then
+       MovementSwitch = 1
+       elseif MovS3 == 2 then
+       MovementSwitch = 2
+       elseif MovS3 == 3 then
+       MovementSwitch = 4
+       elseif MovS3 == 4 then
+       MovementSwitch = 5
+       elseif MovS3 == 5 then
+       MovementSwitch = 6
+       elseif MovS3 == 6 then
+       MovementSwitch = 7
+       elseif MovS3 == 7 then
+       MovementSwitch = 8
+       end
      elseif MovementSwitch == 4 then
        local Mov4 = math.random(121, 649)
        tblua:KeyDown(0x28)
        tblua:Sleep(Mov4)
        tblua:KeyUp(0x28)
-       c = c + 1
-       s = s + 1
        t = t + 1
-       local MovS4 = math.random(8)
-       MovementSwitch = MovS4
+       local MovS4 = math.random(7)
+       if MovS4 == 1 then
+       MovementSwitch = 1
+       elseif MovS4 == 2 then
+       MovementSwitch = 2
+       elseif MovS4 == 3 then
+       MovementSwitch = 3
+       elseif MovS4 == 4 then
+       MovementSwitch = 5
+       elseif MovS4 == 5 then
+       MovementSwitch = 6
+       elseif MovS4 == 6 then
+       MovementSwitch = 7
+       elseif MovS4 == 7 then
+       MovementSwitch = 8
+       end
      elseif MovementSwitch == 5 then
        local Mov5 = math.random(118, 664)
        tblua:KeyDown(0x28)
@@ -164,11 +210,23 @@ if tblua:IsInWorld() == true then
        tblua:Sleep(Mov5)
        tblua:KeyUp(0x28)
        tblua:KeyUp(0x25)
-       c = c + 1
-       s = s + 1
        t = t + 1
-       local MovS5 = math.random(8)
-       MovementSwitch = MovS5
+       local MovS5 = math.random(7)
+       if MovS5 == 1 then
+       MovementSwitch = 1
+       elseif MovS5 == 2 then
+       MovementSwitch = 2
+       elseif MovS5 == 3 then
+       MovementSwitch = 3
+       elseif MovS5 == 4 then
+       MovementSwitch = 4
+       elseif MovS5 == 5 then
+       MovementSwitch = 6
+       elseif MovS5 == 6 then
+       MovementSwitch = 7
+       elseif MovS5 == 7 then
+       MovementSwitch = 8
+       end
      elseif MovementSwitch == 6 then
        local Mov6 = math.random(124, 662)
        tblua:KeyDown(0x28)
@@ -176,11 +234,23 @@ if tblua:IsInWorld() == true then
        tblua:Sleep(Mov6)
        tblua:KeyUp(0x28)
        tblua:KeyUp(0x27)
-       c = c + 1
-       s = s + 1
        t = t + 1
-       local MovS6 = math.random(8)
-       MovementSwitch = MovS6
+       local MovS6 = math.random(7)
+       if MovS6 == 1 then
+       MovementSwitch = 1
+       elseif MovS6 == 2 then
+       MovementSwitch = 2
+       elseif MovS6 == 3 then
+       MovementSwitch = 3
+       elseif MovS6 == 4 then
+       MovementSwitch = 4
+       elseif MovS6 == 5 then
+       MovementSwitch = 5
+       elseif MovS6 == 6 then
+       MovementSwitch = 7
+       elseif MovS6 == 7 then
+       MovementSwitch = 8
+       end
      elseif MovementSwitch == 7 then
        local Mov7 = math.random(117, 665)
        tblua:KeyDown(0x26)
@@ -188,11 +258,23 @@ if tblua:IsInWorld() == true then
        tblua:Sleep(Mov7)
        tblua:KeyUp(0x26)
        tblua:KeyUp(0x27)
-       c = c + 1
-       s = s + 1
        t = t + 1
-       local MovS7 = math.random(8)
-       MovementSwitch = MovS7
+       local MovS7 = math.random(7)
+       if MovS7 == 1 then
+       MovementSwitch = 1
+       elseif MovS7 == 2 then
+       MovementSwitch = 2
+       elseif MovS7 == 3 then
+       MovementSwitch = 3
+       elseif MovS7 == 4 then
+       MovementSwitch = 4
+       elseif MovS7 == 5 then
+       MovementSwitch = 5
+       elseif MovS7 == 6 then
+       MovementSwitch = 6
+       elseif MovS7 == 7 then
+       MovementSwitch = 8
+       end
      elseif MovementSwitch == 8 then
        local Mov8 = math.random(121, 668)
        tblua:KeyDown(0x26)
@@ -200,11 +282,23 @@ if tblua:IsInWorld() == true then
        tblua:Sleep(Mov8)
        tblua:KeyUp(0x26)
        tblua:KeyUp(0x25)
-       c = c + 1
-       s = s + 1
        t = t + 1
-       local MovS8 = math.random(8)
-       MovementSwitch = MovS8
+       local MovS8 = math.random(7)
+       if MovS8 == 1 then
+       MovementSwitch = 1
+       elseif MovS8 == 2 then
+       MovementSwitch = 2
+       elseif MovS8 == 3 then
+       MovementSwitch = 3
+       elseif MovS8 == 4 then
+       MovementSwitch = 4
+       elseif MovS8 == 5 then
+       MovementSwitch = 5
+       elseif MovS8 == 6 then
+       MovementSwitch = 6
+       elseif MovS8 == 7 then
+       MovementSwitch = 7
+       end
     end
   end
  end
