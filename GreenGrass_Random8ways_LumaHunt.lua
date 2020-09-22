4958434c75bf0ddacc698927e8aef0c583eba14f
MaeBot <69312348+MaeBot@users.noreply.github.com>
2020-09-22 10:48:07 +0200

    Update GreenGrass_Random8ways_LumaHunt.lua

--- GreenGrass_Random8ways_LumaHunt.lua
+++ GreenGrass_Random8ways_LumaHunt.lua
@@ -1,7 +1,7 @@
 -- Testing
 
 ----------------------------------------------------
------ Random 4 ways Green Grass : Wait on luma -----
+----- Random 8 ways Green Grass : Wait on luma -----
 ----- Created by MaeBot for TemBot by NhMarco ------
 ----------------------------------------------------
 
